69d5491fdf6ddb12b18fc8815482c87d7db92eac
Aaron Penne <aaronpenne@users.noreply.github.com>
2017-12-12 08:58:29 -0800

    Added CSS style and SVG metadata printing

--- primes/primes.py
+++ primes/primes.py
@@ -18,21 +18,29 @@ colors = ['ffffff',  # 0 white
           'e6194b'   # 9 red
           ]
 
+# Reads in text file containing list of prime numbers, one per line
+with open('primes.txt') as file_in:
+    primes = file_in.read().splitlines()
+
+# Reshapes primes into square list of lists with one char per entry
+for ii in range(rows):
+    for jj in range(cols):
+        # FIXME
+
 # Print svg metadata
-output = open('primes.svg', 'w')
-output.write("<?xml version=\"1.1\" encoding=\"utf-8\"?>\n")
-output.write("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n")
-output.write("<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xml:space=\"preserve\">\n")
+file_out = open('primes.svg', 'w')
+file_out.write("<?xml version=\"1.1\" encoding=\"utf-8\"?>\n")
+file_out.write("<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n")
+file_out.write("<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xml:space=\"preserve\">\n")
 
-# Prints colors in CSS
-output.write("<style type=\"text/css\" id=\"style_css_sheet\">\n")
-for color in colors:
-    output.write() # FIXME
-
-output.write("</style>\n")
+# Prints colors in CSS style block
+file_out.write("<style type=\"text/css\" id=\"style_css_sheet\">\n")
+for index, color in enumerate(colors):
+    file_out.write('\t.class_{0:d} {{fill:#{1:s}}}\n'.format(index, color))
+file_out.write("</style>\n")
 
 
 # Prints one line per digit
 
-output.write("</svg>\n")
-output.close()
+file_out.write("</svg>\n")
+file_out.close()
