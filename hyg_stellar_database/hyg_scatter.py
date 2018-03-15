e8b4f1a4db340e2968f9e4f64f69bae180103bf5
aaronpenne <aaronpenne@users.noreply.github.com>
2018-03-15 09:25:58 -0700

    Plot formatted to look nice, sizes look good.

--- hyg_stellar_database/hyg_scatter.py
+++ hyg_stellar_database/hyg_scatter.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import os
 
 min_mag = -20
-max_mag = 8
+max_mag = 20
 steps = 40
 
 # Set output directory, make it if needed
@@ -22,51 +22,35 @@ if not os.path.isdir(output_dir):
 input_file = os.path.relpath(r'data\hygdata_v3.csv')
 df = pd.read_csv(input_file)
 
-# Only use stars visible to the human eyeish and ignore the sun
+# Filter to certain magnitudes, ignore the sun
 df = df[df.mag > min_mag]
 df = df[df.mag < max_mag]
 
-#df = df[df.ra > 10]
-#df = df[df.ra < 0.1]
-##
-#df = df[df.dec > -78]
-#df = df[df.dec < -76]
-
-# Filter the size/alpha of each magnitude bin (log scale)
+# Filter the size/alpha of each marker by magnitude bin (log scale)
 mag = {}
 for i,value in enumerate(np.geomspace(abs(min_mag)+max_mag, abs(min_mag), num=steps)):
-    mag[i] = df['mag'] >= (value + min_mag)
+    mag[i] = df['mag'] <= (value + min_mag)
 marker = {}
-for i,value in enumerate(np.geomspace(4, 0.0001, num=steps)):
+for i,value in enumerate(np.geomspace(3, 0.1, num=steps)):
     marker[i] = value
 alpha = {}
-for i,value in enumerate(np.geomspace(80, 10, num=steps)):
+for i,value in enumerate(np.geomspace(1, 0.3, num=steps)):
     alpha[i] = value
+    
 
 
-
-
-
-
-
-
-# FIXME left off here
+# Workaround to get each magnitude bin only plotted once
 mag_xor = {}
 for i in range(len(mag)):
     if i == len(mag)-1:
         mag_xor[i] = mag[i]
         break
     mag_xor[i] = mag[i] & (mag[i] ^ mag[i+1])
-
 mag = mag_xor
 
 
-
-
-
-    
-# Set up plot size
-fig, ax = plt.subplots(figsize=(10, 5), dpi=500)
+# Set up plot
+fig, ax = plt.subplots(figsize=(10, 5), dpi=300)
 
 # Black out the entire background
 fig.set_facecolor('black')
@@ -85,6 +69,18 @@ for i in range(steps):
              alpha=alpha[i],
              markeredgewidth=0)
 
+## Why not make constellations pop?
+#x = df.loc[df['bf'].notnull(), 'ra']
+#y = df.loc[df['bf'].notnull(), 'dec']
+#plt.plot(x, y, 
+#         color='white', 
+#         linestyle='none',
+#         linewidth=0,
+#         marker='.', 
+#         markersize=5,
+#         alpha=1,
+#         markeredgewidth=0)
+
 # Despine plot
 for side in ['right', 'left', 'top', 'bottom']:
     ax.spines[side].set_visible(False)
@@ -97,6 +93,30 @@ ax.yaxis.set_ticks([])
 plt.ylim([-90, 90])
 plt.xlim([0, 24])
 
+# Add text
+plt.text(0, 111, 'The Night Sky',
+         family='monospace',
+         size=10,
+         horizontalalignment='left',
+         weight='bold',
+         color='white',
+         alpha=0.6)
+plt.text(0, 105, 'Right Ascension vs. Declination',
+         family='monospace',
+         size=7,
+         horizontalalignment='left',
+         weight='bold',
+         color='white',
+         alpha=0.6)
+plt.text(24, -111, '© 2018 Aaron Penne\nData: HYG Stellar Database',
+         family='monospace',
+         size=7,
+         horizontalalignment='right',
+         weight='bold',
+         color='white',
+         alpha=0.6)
+
+
 # Show plot
 plt.show()
 
@@ -106,4 +126,4 @@ fig.savefig(os.path.join(output_dir, 'hyg_scatter.png'),
             facecolor=fig.get_facecolor(),
             edgecolor='none',
             bbox_inches='tight',
-            pad_inches=0.3)
+            pad_inches=0.7)
