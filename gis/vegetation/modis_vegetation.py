952b1b40328d77c3ba5d1475e3fa0c22ac270260
aaronpenne <aaronpenne@users.noreply.github.com>
2018-03-30 08:59:10 -0700

    Added continent cropping.

--- gis/vegetation/modis_vegetation.py
+++ gis/vegetation/modis_vegetation.py
@@ -26,7 +26,7 @@ import requests
 output_dir = os.path.relpath('output')
 if not os.path.isdir(output_dir):
     os.mkdir(output_dir)
-    
+
 output_dir_raw = os.path.join('output', '_raw')
 if not os.path.isdir(output_dir_raw):
     os.mkdir(output_dir_raw)
@@ -38,28 +38,32 @@ if not os.path.isdir(output_dir_gif):
 output_dir_mp4 = os.path.join('output', '_mp4')
 if not os.path.isdir(output_dir_mp4):
     os.mkdir(output_dir_mp4)
+    
+output_dir_na = os.path.join('output', '_north_america')
+if not os.path.isdir(output_dir_na):
+    os.mkdir(output_dir_na)
 
 def bulk_download(args):
     try:
         root_url = args[0]
         image_date = args[1]
         image_date_clean = image_date.replace('.', '')
-        
+
         image_files = [f for f in os.listdir(output_dir_raw) if image_date_clean in f and f.endswith('jpg')]
         if image_files:
             return
-        
+
         image_dir_url = root_url + image_date
         image_dir_page = requests.get(image_dir_url)
         image_dir_contents = html.fromstring(image_dir_page.content)
         links = image_dir_contents.xpath('//a/@href')
         evi = links[7]
         ndvi = links[8]
-        
+
         evi_url = image_dir_url + '/' + evi
         evi_out = 'mod13ci_006_evi_' + image_date_clean + '.jpg'
         image_download(evi_url, evi_out)
-        
+
         ndvi_url = image_dir_url + '/' + ndvi
         ndvi_out = 'mod13ci_006_ndvi_' + image_date_clean + '.jpg'
         image_download(ndvi_url, ndvi_out)
@@ -70,40 +74,40 @@ def image_download(img_url, img_out):
     image = requests.get(img_url).content
     with open(os.path.join(output_dir_raw, img_out), 'wb') as f:
         f.write(image)
-        
+
 def bulk_annotate(image_file):
     try:
         image_files = [f for f in os.listdir(output_dir_mp4) if image_file in f and f.endswith('jpg')]
         if image_files:
             return
-        
+
         image = Image.open(os.path.join(output_dir_raw, image_file))
-        
+
         # Make the ocean black \m/
         image_rgba = image.convert('RGBA')
         data = np.array(image_rgba)
         r, g, b, a = data.T
         ocean = (r<50) & (b<160) & (b>50)
-        data[..., :-1][ocean.T] = (0, 0, 0)
+        data[..., :-1][ocean.T] = (33, 33, 33)
         image = Image.fromarray(data)
         image = image.convert('RGB')
-        
+
         draw = ImageDraw.Draw(image)
-        
+
         # Add title
         title = 'Enhanced Vegetation Index (EVI) from 2000-2018'
-        consolas_centered(draw, title, (7200, 3150), 117)      
-        
+        consolas_centered(draw, title, (7200, 3150), 117)
+
         # Add dates
         image_date = datetime.strptime(image_file[-12:-4], '%Y%m%d').date()
         date_string = image_date.strftime('%m/%Y')
         consolas_centered(draw, date_string, (7200, 3300), 117)
-        
+
         # Add name and source
-        annotation = 'GIF by Aaron Penne © 2018\nData: NASA MODIS/Terra Vegetation Indices (MOD13C1)\nSource Code: github.com/aaronpenne'
+        annotation = 'Imagery Products: MODIS Science Team\nGIF: Aaron Penne © 2018\nData: NASA MODIS/Terra Vegetation Indices (MOD13C1)\nSource Code: github.com/aaronpenne'
         font = ImageFont.truetype('Consolas.ttf', 50)
-        draw.text((200, 3300), annotation, (177,177,177), font=font)
-#        
+        draw.text((200, 3225), annotation, (177,177,177), font=font)
+#
         # Add slider/text to images
         image.save(os.path.join(output_dir_mp4, 'text_' + image_file),)
     except:
@@ -117,9 +121,18 @@ def bulk_resize(image_file):
         image = Image.open(os.path.join(output_dir_mp4, image_file))
         image.size
         image = image.resize((1903, 978), Image.ANTIALIAS)
-        image.save(os.path.join(output_dir_gif, 'small_' + image_file))    
+        image.save(os.path.join(output_dir_gif, 'small_' + image_file))
     except:
         print('Resize: {}'.format(image_file))
+        
+def bulk_crop(args):
+    image_dir = args[0]
+    image_file = args[1]
+    coords = args[2]
+    
+    image = Image.open(os.path.join(image_dir, image_file))
+    image = image.crop(coords)
+    image.save(os.path.join(output_dir_na, 'na_' + image_file))
 
 def consolas_centered(draw, text, coords=(0,0), size=17):
     font = ImageFont.truetype('Consolas.ttf', size)
@@ -130,17 +143,17 @@ def consolas_centered(draw, text, coords=(0,0), size=17):
     draw.text((x, y), text, (255,255,255), font=font)
 
 if __name__ == '__main__':
-    
-    root_url = 'https://e4ftl01.cr.usgs.gov/MOLT/MOD13C1.006/' 
+
+    root_url = 'https://e4ftl01.cr.usgs.gov/MOLT/MOD13C1.006/'
     root_page = requests.get(root_url)
     root_contents = html.fromstring(root_page.content)
     links = root_contents.xpath('//a/@href')
-    
+
     # Download all images from each image dir in the root dir on the remote server
     image_dates = [i[:-1] for i in links[7:]]
     with Pool(10) as p:
         p.map(bulk_download, zip(itertools.repeat(root_url), image_dates))
-    
+
     # Annotate images
     image_files = [f for f in os.listdir(output_dir_raw) if 'evi' in f and f.endswith('jpg')]
     image_files.sort()
@@ -149,34 +162,45 @@ if __name__ == '__main__':
 #    bulk_annotate(image_files[0])
 
     # Create downsampled copies of images
-    image_files = [f for f in os.listdir(output_dir_mp4) if 'evi' in f and f.endswith('jpg')]
+    image_files = [f for f in os.listdir(output_dir_mp4) if f.endswith('jpg')]
     image_files.sort()
     with Pool(10) as p:
         p.map(bulk_resize, image_files)
-    
+
     # Create downsized gif
-    image_files = [f for f in os.listdir(output_dir_gif) if 'small' in f and f.endswith('jpg')]
+    image_files = [f for f in os.listdir(output_dir_gif) if f.endswith('jpg')]
     image_files.sort()
     image_list = []
     for f in image_files:
         image = imageio.imread(os.path.join(output_dir_gif, f))
         image_list.append(image)
     imageio.mimsave(os.path.join(output_dir, 'animation_evi.gif'), image_list, format='GIF', duration=0.2)
-    
+
     # Create full size movie
-    image_files = [f for f in os.listdir(output_dir_mp4) if 'text' in f and f.endswith('jpg')]
+    image_files = [f for f in os.listdir(output_dir_mp4) if f.endswith('jpg')]
     image_files.sort()
     image_list = []
     for f in image_files:
         image = imageio.imread(os.path.join(output_dir_mp4, f))
         image_list.append(image)
     imageio.mimsave(os.path.join(output_dir, 'animation_evi.mp4'), image_list, format='MP4', fps=5)
-    
 
 
-    
+    # Create North America zoom
+    image_files = [f for f in os.listdir(output_dir_mp4) if f.endswith('jpg')]
+    image_files.sort()
+    with Pool(10) as p:
+        p.map(bulk_crop, zip(itertools.repeat(output_dir_mp4), image_files, itertools.repeat((0, 150, 2635, 1690))))
+        
+    image_files = [f for f in os.listdir(output_dir_na) if f.endswith('jpg')]
+    image_files.sort()
+    image_list = []   
+    for f in image_files:      
+        image = imageio.imread(os.path.join(output_dir_na, f))
+        image_list.append(image)
+    imageio.mimsave(os.path.join(output_dir, 'animation_evi_na.mp4'), image_list, format='MP4', fps=5)
+
     # FIXME:
-    # Replace blue in ocean with black
     # DO it all again for NDVI for comparison
     # Do zoom continents/countries
     # Do do all Dec and Jul for YOY comparison
