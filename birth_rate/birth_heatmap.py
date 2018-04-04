d224fc6f86a6d26fa69d0f4996dccf9020a88e36
aaronpenne <aaronpenne@users.noreply.github.com>
2018-04-04 12:47:34 -0700

    Birth rate heatmaps normalized and finished.

--- birth_rate/birth_heatmap.py
+++ birth_rate/birth_heatmap.py
@@ -13,6 +13,7 @@ Developed with:
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib as mp
 import matplotlib.pyplot as plt
 import os
 
@@ -27,9 +28,30 @@ def get_data_csv(filename):
     df = pd.read_csv(input_file)
     return df
 
+def round_to_n(x, n, direction=''):
+    direction = direction.lower()
+    if direction == 'up':
+        return int(n*np.ceil(float(x)/n))
+    elif direction == 'down':
+        return int(n*np.floor(float(x)/n))
+    else:
+        return int(n*round(float(x)/n))
+    
+def get_custom_tick_labels(dp, base=5):
+    x_names = [min(dp)]
+    for i in range(int((round_to_n(max(dp),base,'down')-round_to_n(min(dp),base,'up'))/base)+1):
+        x_names.append(round_to_n(min(dp),base,'up')+i*base)
+    if x_names[-1] != max(dp):
+        x_names.append(max(dp))
+    
+    x_values = []
+    for name in x_names:
+        x_values.append(dp.columns.get_loc(name))
+        
+    return x_values, x_names
 
 min_year = 1975
-max_year = 2018
+max_year = 2015
 
 
 #------------------------------------------------------------------------------
@@ -86,74 +108,135 @@ df = df.loc[df.index <= df_birth.index.max()]
 
 
 sns.set_style('whitegrid')
+month_names = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 
-# Rate plot for grounding
+h1 = mp.font_manager.FontProperties(family='monospace', size='large')
+h2 = mp.font_manager.FontProperties(family='monospace', size='small')
+h3 = mp.font_manager.FontProperties(family='monospace', size='x-small')
+
+
+#
+# Birth rate
+#
+
+# Line - raw
 dp = df
-
 fig, ax = plt.subplots(figsize=(12, 4))
 ax.plot(dp['rate'])
 
-# Boxplot with dots (month to month comparison)
+# Boxplot - with dots (month to month comparison)
 dp = df
-
 fig, ax = plt.subplots(figsize=(12, 4))
-sns.boxplot(x='month', y='rate', data=dp, ax=ax, palette='plasma')
+sns.boxplot(x='month', y='rate', data=dp, ax=ax, palette='YlOrRd')
 sns.stripplot(x='month', y='rate', data=dp, ax=ax, color='0', jitter=0.1)
 
-# Heatmap - full data
-dp = pd.pivot_table(df, index='month', columns='year', values='rate', aggfunc=np.sum)
-
-fig, ax = plt.subplots(figsize=(12, 4))
-cbar_ax = fig.add_axes([0.93, .33, .01, .33])
-sns.heatmap(dp,
-            ax = ax,
-            cbar_ax = cbar_ax,
-            yticklabels=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            xticklabels=2,
-            cmap="plasma",
-            square=True,
-            cbar=True)
-ax.set_xlabel('Year', 
-            fontsize=16,
-            fontname = 'monospace')
-ax.set_ylabel('')
-ax.tick_params(axis='both', which='major', labelsize=9)
-
-# Heatmap - ranged data
-dp = df
-dp = dp.loc[dp.index.to_series().dt.year >= min_year]
-dp = dp.loc[dp.index.to_series().dt.year <= max_year]
-dp = pd.pivot_table(dp, index='month', columns='year', values='rate', aggfunc=np.sum)
-
-fig, ax = plt.subplots(figsize=(12, 4))
-cbar_ax = fig.add_axes([0.93, .33, .01, .33])
-sns.heatmap(dp,
-            ax = ax,
-            cbar_ax = cbar_ax,
-            yticklabels=['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-            xticklabels=2,
-            cmap="plasma",
-            square=True,
-            cbar=True)
-ax.set_xlabel('Year', 
-            fontsize=16,
-            fontname = 'monospace')
-ax.set_ylabel('')
-ax.tick_params(axis='both', which='major', labelsize=9)
-
-# Plot each month over the years
+    
+# Line - each month over years
 dp = df
 dp = pd.pivot_table(dp, index='month', columns='year', values='rate', aggfunc=np.sum)
-
 fig, ax = plt.subplots(figsize=(12, 4))
 for index, row in dp.iterrows():
     ax.plot(row)
     
-# Plot max/min of each year over the years
+# Line - max/min of each year over the years
 dp = df
 dp = pd.pivot_table(dp, index='month', columns='year', values='rate', aggfunc=np.sum)
-
 fig, ax = plt.subplots(figsize=(12, 4))
 for year in df:
     ax.plot(dp.max())
     ax.plot(dp.min())
+    
+    
+# Heatmap - matplotlib
+dp = pd.pivot_table(df, index='month', columns='year', values='rate', aggfunc=np.sum)
+fig, ax = plt.subplots(figsize=(12, 4), dpi=150)
+plt.imshow(dp, interpolation='nearest', cmap='YlOrRd')
+ax.grid(False)
+for _, loc in ax.spines.items():
+#    loc.set_visible(True)
+    loc.set_color('black')
+plt.yticks(np.linspace(0,11,12), month_names,
+           family='monospace',
+           size='small',
+           color='black')
+x_values, x_names = get_custom_tick_labels(dp, 2)
+plt.xticks(x_values, x_names,
+           family='monospace',
+           size='small',
+           color='black',
+           rotation='vertical')
+cax = fig.add_axes([0.92, .3333, 0.01, .3333])
+cb = plt.colorbar(label='births/day per million ppl', cax=cax)
+cb.outline.set_edgecolor('black')
+cax.yaxis.label.set_font_properties(h3)
+cax.set_yticklabels(cax.get_yticklabels(), 
+                    family='monospace',
+                    size='x-small')
+# Annotations
+ax.text(-0.5, -4.2,
+        'USA Birth Rate Every Month 1933-2015',
+        fontsize = 14,
+        color = 'black',
+        weight = 'bold',
+        family = 'monospace')
+ax.text(-0.5, -2,
+        'Birth rate per day per million people, binned by month',
+        fontsize = 12,
+        color = 'black',
+        family = 'monospace')
+ax.text(64, -2,
+        'Birth data: Human Mortality Database\nPopulation data: US Census Bureau\nCode: www.github.com\\aaronpenne\nAaron Penne © 2018',
+        fontsize = 7,
+        color = 'gray',
+        family = 'monospace',
+        multialignment = 'right')
+fig.savefig('birth_rate_usa.png', dpi='figure', bbox_inches='tight', pad_inches=.11)
+
+
+# Heatmap - matplotlib - ranged
+dp = df
+dp = dp.loc[dp.index.to_series().dt.year >= min_year]
+dp = dp.loc[dp.index.to_series().dt.year <= max_year]
+dp = pd.pivot_table(dp, index='month', columns='year', values='rate', aggfunc=np.sum)
+fig, ax = plt.subplots(figsize=(8, 4), dpi=150)
+plt.imshow(dp, interpolation='nearest', cmap='YlOrRd')
+ax.grid(False)
+for _, loc in ax.spines.items():
+#    loc.set_visible(True)
+    loc.set_color('black')
+plt.yticks(np.linspace(0,11,12), month_names,
+           family='monospace',
+           size='small',
+           color='black')
+x_values, x_names = get_custom_tick_labels(dp, 2)
+plt.xticks(x_values, x_names,
+           family='monospace',
+           size='small',
+           color='black',
+           rotation='vertical')
+cax = fig.add_axes([0.92, .28, 0.01, .45])
+cb = plt.colorbar(label='births/day per million ppl', cax=cax)
+cb.outline.set_edgecolor('black')
+cax.yaxis.label.set_font_properties(h3)
+cax.set_yticklabels(cax.get_yticklabels(), 
+                    family='monospace',
+                    size='x-small')
+# Annotations
+ax.text(-0.5, -3.5,
+        'USA Birth Rate Every Month ' + str(min_year) + '-' + str(max_year),
+        fontsize = 12,
+        color = 'black',
+        weight = 'bold',
+        family = 'monospace')
+ax.text(-0.5, -2,
+        'Birth rate per day per million people, binned by month',
+        fontsize = 10,
+        color = 'black',
+        family = 'monospace')
+ax.text(34, -2,
+        'Birth data: Human Mortality Database\nPopulation data: US Census Bureau\nCode: www.github.com\\aaronpenne\nAaron Penne © 2018',
+        fontsize = 6,
+        color = 'gray',
+        family = 'monospace',
+        multialignment = 'right')
+fig.savefig('birth_rate_usa_ranged.png', dpi='figure', bbox_inches='tight', pad_inches=.11)
